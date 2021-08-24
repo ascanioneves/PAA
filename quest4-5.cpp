cc6a174466f0138b044948bb52fde4672ff49a68
Ascanio Savio <ascaniosavio97@gmail.com>
2021-08-24 15:11:06 -0300

    Update quest4-5.cpp

--- quest4-5.cpp
+++ quest4-5.cpp
@@ -11,7 +11,7 @@ int partyAlgo(int numPeople){
 	int input, accepted = 0;
 
    //Criando as relações
-	for(int i=0;i<numPeople;i++){
+   for(int i=0;i<numPeople;i++){
       cout << "Quem a " << G.getPerson(i) << " conhece? Digite -1 para terminar.\n";
       do{
          cout << "Pessoa #: ";
@@ -19,12 +19,12 @@ int partyAlgo(int numPeople){
 
          //Se é valido adicionamos a matriz de adjacencia
          if(input > 0 && input <= numPeople && input != i+1)
-			{
-				if (G.isEdge(i,input-1))
-					cout << "Pessoa " << i+1 << " e Pessoa " << input << " ja estão conectadas." << endl;
-				else
-					G.addEdge(i,input-1);
-			}
+	 {
+		if (G.isEdge(i,input-1))
+			cout << "Pessoa " << i+1 << " e Pessoa " << input << " ja estão conectadas." << endl;
+		else
+			G.addEdge(i,input-1);
+	 }
          else if (input == i+1)
             cout << "Esse numero pertence a ele mesmo!" << endl;
          else if (input == -1)
@@ -36,44 +36,44 @@ int partyAlgo(int numPeople){
    }
 
 	//Primeiros convites
-	for (int i=0;i<numPeople;i++){
-		invite[i] = 1; //começamos convidando todos
-		if (G.getDegree(i) < 5)
-		{
-			rejects.push(i); //essa pessoa é rejeitada
-			invite[i] = 0; //Cancelando o convite de quem conhece menos de 5
-		}
-	}
-
-	//Garantindo que todos conhecem mais de 5
-	while(!rejects.empty())
+   for (int i=0;i<numPeople;i++){
+	invite[i] = 1; //começamos convidando todos
+	if (G.getDegree(i) < 5)
 	{
-		int j = rejects.front();
-		rejects.pop();
+		rejects.push(i); //essa pessoa é rejeitada
+		invite[i] = 0; //Cancelando o convite de quem conhece menos de 5
+	}
+   }
 
-		for(int i=0;i<numPeople;i++)
+   //Garantindo que todos conhecem mais de 5
+   while(!rejects.empty())
+   {
+	int j = rejects.front();
+	rejects.pop();
+
+	for(int i=0;i<numPeople;i++)
+	{
+		G.removeEdge(i,j);
+		if (G.getDegree(i) < 5 && invite[i] == 1)
 		{
-			G.removeEdge(i,j);
-			if (G.getDegree(i) < 5 && invite[i] == 1)
-			{
-				rejects.push(i);
-				invite[i] = 0;
-			}
+			rejects.push(i);
+			invite[i] = 0;
 		}
 	}
+   }
 
 	//Contagem final de convidados
-	cout << endl;
-	for (int i=0;i<numPeople;i++)
-	{
-		if (invite[i] == 1)
-		{
-			cout << G.getPerson(i) << " pode vir\n";
-			accepted++;
-		}
+   cout << endl;
+   for (int i=0;i<numPeople;i++)
+   {
+   	if (invite[i] == 1)
+   	{
+		cout << G.getPerson(i) << " pode vir\n";
+		accepted++;
 	}
+   }
 
-	return accepted;
+   return accepted;
 }
 
 int main()
